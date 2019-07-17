ceb41e6bf720aead17e09947e14f6003500c6f0d
Keita Yamamoto <yamamoto@altonotes.co.jp>
2019-07-17 15:06:03 +0900

    first commit

---
+++ TraderChartTutorial/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TraderChartTutorial
+//
+//  Created by Keita Yamamoto on 2019/07/17.
+//  Copyright © 2019 altonotes Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TraderChartTutorial/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TraderChartTutorial
+//
+//  Created by Keita Yamamoto on 2019/07/17.
+//  Copyright © 2019 altonotes Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
