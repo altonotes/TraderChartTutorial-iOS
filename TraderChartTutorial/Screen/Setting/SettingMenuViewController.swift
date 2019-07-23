687e59c7ae72bd27c2f618c44ad8f3c785286751
Keita Yamamoto <yamamoto@altonotes.co.jp>
2019-07-23 16:13:02 +0900

    ファイル追加

---
+++ TraderChartTutorial/Screen/Setting/SettingMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingMenuViewController.swift
+//  TraderChartTutorial
+//
+//  Created by Keita Yamamoto on 2019/07/23.
+//  Copyright © 2019 altonotes Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
